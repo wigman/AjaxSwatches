7342421373d1f8fd70cacfa35a75ff5715e6ef7d
avra911 <razvan@nrj.ro>
2015-11-27 11:34:56 +0200

    Added compatibility withy Amasty Stockstatus

--- app/code/community/Wigman/AjaxSwatches/Block/Catalog/Product/View/Type/Configurable/Abstract.php
+++ app/code/community/Wigman/AjaxSwatches/Block/Catalog/Product/View/Type/Configurable/Abstract.php
@@ -1,6 +1,8 @@
 <?php
 if ((string)Mage::getConfig()->getModuleConfig('OrganicInternet_SimpleConfigurableProducts')->active == 'true'){
     class Wigman_AjaxSwatches_Block_Catalog_Product_View_Type_Configurable_Abstract extends OrganicInternet_SimpleConfigurableProducts_Catalog_Block_Product_View_Type_Configurable {}
+} else if ((string)Mage::getConfig()->getModuleConfig('Amasty_Stockstatus')->active == 'true') {
+    class Wigman_AjaxSwatches_Block_Catalog_Product_View_Type_Configurable_Abstract extends Amasty_Stockstatus_Block_Rewrite_Product_View_Type_Configurable {}
 } else {
     class Wigman_AjaxSwatches_Block_Catalog_Product_View_Type_Configurable_Abstract extends Mage_Catalog_Block_Product_View_Type_Configurable {}
 }
